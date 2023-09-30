b55e3f094f088c002654459bddc070c0a79e8168
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-09-30 01:37:36 -0400

    Initial project setup.

---
+++ Highway.Server/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Highway.Server;
+
+public class Program
+{
+    /// <summary>
+    /// Runs the program.
+    /// </summary>
+    /// <param name="args">Arguments from the command line.</param>
+    public static void Main(string[] args)
+    {
+        // TODO: Implement.
+    }
+}
