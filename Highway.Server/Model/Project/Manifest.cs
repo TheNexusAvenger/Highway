94c43e886beae206dc75794e3b1d9e3d33b4ecac
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-09-30 01:49:46 -0400

    Create initial file system models.

---
+++ Highway.Server/Model/Project/Manifest.cs
@@ -0,0 +1,24 @@
+﻿namespace Highway.Server.Model.Project;
+
+public class Manifest
+{
+    /// <summary>
+    /// Optional display name of the project.
+    /// </summary>
+    public string? Name { get; set; }
+    
+    /// <summary>
+    /// Optional place id to require for pulling/pushing changes.
+    /// </summary>
+    public long? PushPlaceId { get; set; }
+    
+    /// <summary>
+    /// Optional place id to require for live syncing changes.
+    /// </summary>
+    public long? SyncPlaceId { get; set; }
+
+    /// <summary>
+    /// Dictionary of the Studio paths to the file system paths to sync.
+    /// </summary>
+    public Dictionary<string, string> Paths { get; set; } = null!;
+}
---
+++ Highway.Server/Model/State/ScriptHashCollection.cs
@@ -0,0 +1,15 @@
+﻿namespace Highway.Server.Model.State;
+
+public class ScriptHashCollection
+{
+    /// <summary>
+    /// Hash method used for the collection.
+    /// It will probably stay SHA256, but it is here in case it changes.
+    /// </summary>
+    public string HashMethod { get; set; } = "SHA256";
+
+    /// <summary>
+    /// Current hashes of the script contents in Roblox Studio.
+    /// </summary>
+    public Dictionary<string, string> Hashes { get; set; } = new Dictionary<string, string>();
+}
