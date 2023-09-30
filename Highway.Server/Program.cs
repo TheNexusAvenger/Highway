6a3fac4261e5c44a1f0e1aef5be82e3233c195b7
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2023-09-30 01:57:08 -0400

    Add logger and create initial ASP.NET server.

--- Highway.Server/Program.cs
+++ Highway.Server/Program.cs
@@ -1,13 +1,44 @@
-﻿namespace Highway.Server;
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace Highway.Server;
 
 public class Program
 {
+    /// <summary>
+    /// Default port to use.
+    /// </summary>
+    public const ushort Port = 22894;
+    
     /// <summary>
     /// Runs the program.
     /// </summary>
     /// <param name="args">Arguments from the command line.</param>
     public static void Main(string[] args)
     {
-        // TODO: Implement.
+        // TODO: Support command line arguments to enable debug and not start server.
+        // Build the server.
+        Logger.Debug("Preparing web server.");
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Logging.ClearProviders();
+        builder.Logging.AddProvider(Logger.NexusLogger);
+        builder.Services.AddControllers();
+            
+        // Start the server.
+        var app = builder.Build();
+        app.UseExceptionHandler(exceptionHandlerApp =>
+        {
+            exceptionHandlerApp.Run(context =>
+            {
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                if (exceptionHandlerPathFeature != null)
+                {
+                    Logger.Error($"An exception occurred processing {context.Request.Method} {context.Request.Path}\n{exceptionHandlerPathFeature.Error}");
+                }
+                return Task.CompletedTask;
+            });
+        });
+        app.MapControllers();
+        Logger.Info($"Starting server on port {Port}.");
+        app.Run($"http://*:{Port}");
     }
 }
